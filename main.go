940f847052b03945a5771e0466c7738ebcc38ede
joey <joey.douglas@gmail.com>
2019-10-24 18:14:08 -0500

    added OauthRequest type and methods. Moved logic into methods from func main

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 
-	"encoding/json"
+	"github.com/google/uuid"
 
-	"context"
+	"strconv"
 
 	"golang.org/x/oauth2"
 )
@@ -26,77 +28,69 @@ type DropboxConfig struct {
 	RedirectURL string `json:"redirect_url"`
 }
 
+type OauthRequest struct {
+	JSONConfig  *Config
+	OauthConfig *oauth2.Config
+}
+
+//IsProd method checks if the environment is Prod. If so, it might eventually do something.
 func (c Config) IsProd() bool {
 	return c.Env == "prod"
 }
 
-func DefaultConfig() Config {
-	return Config{
-		Port: 3000,
-		Env:  "dev",
+//OautCallback method receives the Activation Code from the IDP, then exchanges it for the Token.
+func (o *OauthRequest) OauthCallback(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintln(w, "", "code:", r.FormValue("code"), "state:", r.FormValue("state"))
+
+	code := r.FormValue("code")
+	token, err := o.OauthConfig.Exchange(context.TODO(), code)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
+	fmt.Fprintln(w, "token:", token.AccessToken)
+	fmt.Fprintln(w, "type:", token.TokenType)
+	fmt.Fprintln(w, "refresh token:", token.RefreshToken)
+	fmt.Fprintln(w, "expiryf:", token.Expiry)
+}
+
+//OauthConnect method sets a random state, then connects to the IDP's AuthCodeURL provided in the config file.
+func (o *OauthRequest) OauthConnect(w http.ResponseWriter, r *http.Request) {
+	state, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Println("Error generating random UUID for state: ", err)
+	}
+	fmt.Println(state)
+	url := o.OauthConfig.AuthCodeURL(state.String())
+	http.Redirect(w, r, url, http.StatusFound)
 }
 
 func main() {
 
+	//Load configuration
 	cfg := loadConfig()
-	dbxOauth := &oauth2.Config{
-		ClientID:     cfg.Dropbox.ID,
-		ClientSecret: cfg.Dropbox.Secret,
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  cfg.Dropbox.AuthURL,
-			TokenURL: cfg.Dropbox.TokenURL,
+
+	//Create an OauthRequest object and populate it with configuration data.
+	oauth2req := OauthRequest{
+		OauthConfig: &oauth2.Config{
+			ClientID:     cfg.Dropbox.ID,
+			ClientSecret: cfg.Dropbox.Secret,
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  cfg.Dropbox.AuthURL,
+				TokenURL: cfg.Dropbox.TokenURL,
+			},
+			RedirectURL: cfg.Dropbox.RedirectURL,
 		},
-		RedirectURL: cfg.Dropbox.RedirectURL,
 	}
 
-	dbxRedirect := func(w http.ResponseWriter, r *http.Request) {
-		state := "TODO-CREATE_STATE"
-		fmt.Println(state)
-		url := dbxOauth.AuthCodeURL(state)
-		http.Redirect(w, r, url, http.StatusFound)
-	}
-
-	http.HandleFunc("/oauth/dropbox/connect", dbxRedirect)
-
-	http.HandleFunc("/authorize", authorize)
-
-	dbxCallback := func(w http.ResponseWriter, r *http.Request) {
-		r.ParseForm()
-		fmt.Fprintln(w, "", "code:", r.FormValue("code"), "state:", r.FormValue("state"))
-
-		code := r.FormValue("code")
-		token, err := dbxOauth.Exchange(context.TODO(), code)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-		fmt.Fprintln(w, "token:", token.AccessToken)
-		fmt.Fprintln(w, "type:", token.TokenType)
-		fmt.Fprintln(w, "refresh token:", token.RefreshToken)
-		fmt.Fprintln(w, "expiryf:", token.Expiry)
-	}
-	http.HandleFunc("/oauth/dropbox/callback", dbxCallback)
-	http.ListenAndServe(":3000", nil)
+	//Setup HTTP server and routing.
+	http.HandleFunc("/oauth/dropbox/connect", oauth2req.OauthConnect)
+	http.HandleFunc("/oauth/dropbox/callback", oauth2req.OauthCallback)
+	http.ListenAndServe(":"+strconv.Itoa(cfg.Port), nil)
 
 }
 
-func authorize(w http.ResponseWriter, r *http.Request) {
-
-	// cfg := loadConfig()
-	// dbxOauth := &oauth2.Config{
-	// 	ClientID:     cfg.Dropbox.ID,
-	// 	ClientSecret: cfg.Dropbox.Secret,
-	// 	Endpoint: oauth2.Endpoint{
-	// 		AuthURL:  cfg.Dropbox.AuthURL,
-	// 		TokenURL: cfg.Dropbox.TokenURL,
-	// 	},
-	// 	RedirectURL: cfg.Dropbox.RedirectURL,
-	// }
-
-	// fmt.Fprintf(w, "", cfg)
-
-}
-
+//loadConfig loads configuration from file.
 func loadConfig() Config {
 	file, err := os.Open("oauth_svc.conf")
 	if err != nil {
@@ -113,5 +107,12 @@ func loadConfig() Config {
 		panic(err)
 	}
 	return c
+}
 
+//DefaultConfig sets the configuration to use if config file isn't loaded.
+func DefaultConfig() Config {
+	return Config{
+		Port: 3000,
+		Env:  "dev",
+	}
 }
